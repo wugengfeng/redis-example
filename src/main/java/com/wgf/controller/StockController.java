e73b6685b1da76e66ba1b273a9a5a9baeb15848e
wgf <wugengfeng@aukeys.com>
2023-01-29 16:04:21 +0800

    秒杀

--- src/main/java/com/wgf/controller/StockController.java
+++ src/main/java/com/wgf/controller/StockController.java
@@ -8,9 +8,13 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
@@ -23,7 +27,7 @@ import java.util.concurrent.TimeUnit;
  * @create: 2022-02-09 16:58
  **/
 @Slf4j
-@Api(tags = "库存api")
+@Api(tags = "秒杀测试")
 @RestController
 public class StockController {
 
@@ -37,29 +41,30 @@ public class StockController {
     @GetMapping("decr")
     public void decr() {
         Runnable task = () -> {
-            int  num   = getNum();
+            int num = getNum();
             Long stock = redisTemplate.opsForValue().decrement("stock", num);
 
             if (stock < 0) {
                 log.error("扣减库存失败，购买数量：{}", num);
                 redisTemplate.opsForValue().increment("stock", num);
             } else {
-                log.info("库存成功扣减");
+                log.info("库存成功扣减，扣减数量：{}", num);
             }
         };
 
-        for (int i = 0; i < 1500; i++) {
+        for (int i = 0; i < 3000; i++) {
             executorService.execute(task);
         }
 
-        // 测试最总结果： 库存数量还有1的时候，多个线程下单数量为1下单失败，库存数量不能准确控制
+        // 测试最终结果： 库存数量还有1的时候，多个线程下单数量为1下单失败，库存数量不能准确控制
     }
+
 
     @ApiOperation(value = "使用watch实现秒杀")
     @GetMapping("decr2")
     public void decr2() {
         Runnable task = () -> {
-            Boolean flag = decrStock("stock", getNum(), 100);
+            Boolean flag = decrStock("stock", getNum(), 3);
             if (flag) {
                 log.info("下单成功");
             } else {
@@ -70,6 +75,7 @@ public class StockController {
         for (int i = 0; i < 3000; i++) {
             executorService.execute(task);
         }
+        // 测试最终结果： 库存能够被精确扣减，但是执行效率太低，订单失败成功率高
     }
 
 
@@ -106,7 +112,7 @@ public class StockController {
                     result = operations.exec();
 
                     if (Objects.nonNull(result) && result.size() > 0) {
-                        //log.info("剩余库存：{}", result.get(0));
+                        log.info("剩余库存：{}", result.get(0));
                         return true;
                     }
                 }
@@ -117,13 +123,41 @@ public class StockController {
         return result;
     }
 
+    // 定义Lua脚本，库存足够执行INCRBY命令后返回0，库存不够返回0
+    private static RedisScript<Long> STOCK_SCRIPT = new DefaultRedisScript<Long>(
+            "local stock = redis.call('get', KEYS[1])\n" +
+                    "if (stock >= ARGV[1]) then\n" +
+                    "  redis.call('decrby', KEYS[1], ARGV[1])\n" +
+                    "  return 1\n" +
+                    "else\n" +
+                    "  return 0\n" +
+                    "end", Long.class);
+
+    @ApiOperation(value = "使用Lua实现秒杀")
+    @GetMapping("decr3")
+    public void decr3() {
+        Runnable task = () -> {
+            int num = getNum();
+            Long result = (Long) this.redisTemplate.execute(STOCK_SCRIPT, Arrays.asList("stock"), num);
+            if (result > 0) {
+                log.info("下单成功");
+            } else {
+                log.error("下单失败");
+            }
+        };
+
+        for (int i = 0; i < 3000; i++) {
+            executorService.execute(task);
+        }
+    }
+
 
     public static int getNum() {
         int min = 1;
         int max = 20;
 
         Random random = new Random();
-        int    value  = random.nextInt(max + min) + min;
+        int value = random.nextInt(max + min) + min;
 
         return value;
     }
